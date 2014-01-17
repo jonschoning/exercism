d915c42f95b05f239373ecaf3c304e9c37cbb5df
Jon Schoning <jonschoning@gmail.com>
2014-01-17 01:54:46 -0600

    impl haskell/word-count

---
+++ haskell/word-count/WordCount.hs
@@ -0,0 +1,11 @@
+module WordCount (wordCount) where
+
+import Data.Char
+import qualified Data.Map.Lazy as M
+
+wordCount :: String -> M.Map String Int
+wordCount = foldr addToMap M.empty . tokenize
+            where addToMap s = M.insertWith (+) s 1
+                  tokenize = words . map (\c -> if isLetter c || isNumber c then toLower c else ' ')
+
+
