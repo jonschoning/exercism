cbc534f83e8ddaa302c7719aac13886102ce3c4d
Jon Schoning <jonschoning@gmail.com>
2014-01-18 11:09:35 -0600

    impl haskell/anagram

---
+++ haskell/anagram/Anagram.hs
@@ -0,0 +1,15 @@
+module Anagram (anagramsFor) where
+
+import Data.List (sort)
+import Data.Char (toLower)
+
+anagramsFor :: String -> [String] -> [String]
+anagramsFor = filter . isAnagram
+  where isAnagram a b
+          | la == lb  = False
+          | otherwise = sort la == sort lb
+            where
+              la = map toLower a
+              lb = map toLower b
+
+
