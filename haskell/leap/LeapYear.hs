4f16d37ae2de04454e5e91d907da9e796165387d
Jon Schoning <jonschoning@gmail.com>
2014-01-21 00:48:09 -0600

    impl LeapYear.hs

---
+++ haskell/leap/LeapYear.hs
@@ -0,0 +1,8 @@
+module LeapYear (isLeapYear) where
+
+isLeapYear :: Int -> Bool
+isLeapYear y = mod4 && (not mod100 || mod100 && mod400)
+  where
+    mod4 = y `mod` 4 == 0
+    mod100 = y `mod` 100 == 0
+    mod400 = y `mod` 400 == 0
