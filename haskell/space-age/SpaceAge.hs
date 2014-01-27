0cc1aaccc6c6aea003bbebcf18d414af78688b63
Jon Schoning <jonschoning@gmail.com>
2014-01-26 23:48:31 -0600

    impl spaceage.hs

--- haskell/space-age/SpaceAge.hs
+++ haskell/space-age/SpaceAge.hs
@@ -1,6 +1,8 @@
 module SpaceAge (Planet(..), ageOn) where
 
-data Planet = 
+import qualified Data.Map.Strict as M
+
+data Planet =
     Mercury
   | Venus
   | Earth
@@ -9,10 +11,19 @@ data Planet =
   | Saturn
   | Uranus
   | Neptune
+  deriving (Ord, Eq)
+
+periodFromEarth :: M.Map Planet Float
+periodFromEarth = M.fromList [(Mercury, 0.2408467),
+                              (Venus, 0.61519726),
+                              (Mars, 1.8808158),
+                              (Earth, 1),
+                              (Jupiter, 11.862615),
+                              (Saturn, 29.447498),
+                              (Uranus, 84.016846),
+                              (Neptune, 164.79132)]
 
 ageOn :: Planet -> Int -> Float
-ageOn Earth = secondsToEarthYears 
-ageOn _ = undefined
+ageOn planet = (/ periodFromEarth M.! planet) . toEarthYears 
+  where toEarthYears seconds = fromIntegral seconds / 3.15576e7
 
-secondsToEarthYears :: Int -> Float
-secondsToEarthYears s = fromIntegral s / 3.15576e7
