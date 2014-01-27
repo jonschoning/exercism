023b492a53785196c87615087a7108e3de8e2f93
Jon Schoning <jonschoning@gmail.com>
2014-01-26 22:37:19 -0600

    refactor  ETL.hs

--- haskell/etl/ETL.hs
+++ haskell/etl/ETL.hs
@@ -4,9 +4,6 @@ import qualified Data.Map.Strict as M
 import Data.Char (toLower)
 
 transform :: M.Map Int [String] -> M.Map String Int
-transform m = M.fromList $ swapConcat $ M.toList m
-
-swapConcat :: [(Int, [String])] -> [(String, Int)]
-swapConcat = concatMap (\p -> map (\s -> (lower s, fst p)) $ snd p)
-  where lower = map toLower
+transform = M.fromList . concatMap swap . M.toList
+  where swap (v, xs) = map (\s -> (map toLower s, v)) xs
 
