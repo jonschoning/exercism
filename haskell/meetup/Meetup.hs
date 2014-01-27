952fa15d43ed81dedb5cde8095f2f09d237b1032
Jon Schoning <jonschoning@gmail.com>
2014-01-26 22:23:32 -0600

    simplify Meetup

--- haskell/meetup/Meetup.hs
+++ haskell/meetup/Meetup.hs
@@ -3,6 +3,7 @@ module Meetup (Weekday(..), Schedule(..), meetupDay) where
 import Data.Time.Calendar (Day, fromGregorian, toGregorian, gregorianMonthLength)
 import Data.Time.Format (formatTime)
 import System.Locale (defaultTimeLocale)
+import Control.Monad (liftM2)
 import Data.List (filter)
 
 data Schedule = First | Second | Third | Fourth | Teenth | Last
@@ -13,15 +14,14 @@ data Weekday = Sunday | Monday | Tuesday | Wednesday | Thursday | Friday | Satur
 
 meetupDay :: Schedule -> Weekday -> Integer -> Int -> Day
 meetupDay schedule weekday year month
-  | schedule `elem` enumFromTo First Fourth = head $ 
-                                              drop (fromEnum schedule) $ 
-                                              filter isWeekday fromFirstDayOfMonth
-  | schedule == Last = last $ filter isWeekday $ fromFirstDayOfMonth
-  | schedule == Teenth = head $ filter isWeekday $ filter (isTeenth . toMonthDay . toGregorian) fromFirstDayOfMonth
+  | schedule == Last   = last $ filter isWeekday fromFirstDayOfMonth
+  | schedule == Teenth = head $ filter (liftM2 (&&) isWeekday isDayTeenth) fromFirstDayOfMonth
+  | otherwise          = head $ drop (fromEnum schedule) $ filter isWeekday fromFirstDayOfMonth
   where
+    toMonthDay (_,_,x) = x
+    isTeenth = flip elem [13, 14, 15, 16, 17, 18, 19]
+    isDayTeenth = isTeenth . toMonthDay . toGregorian
+    isWeekday d = formatTime defaultTimeLocale "%A" d == (show weekday)
     fromFirstDayOfMonth = take (gregorianMonthLength year month) $
                           iterate succ $ 
                           fromGregorian year month 1
-    toMonthDay (_,_,x) = x
-    isTeenth = flip elem [13, 14, 15, 16, 17, 18, 19]
-    isWeekday d = formatTime defaultTimeLocale "%A" d == (show weekday)
