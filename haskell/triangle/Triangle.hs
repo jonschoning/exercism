517fb7d7c76788564c3f1a0c9f2ccc749d0c72c0
Jon Schoning <jonschoning@gmail.com>
2014-02-22 15:32:51 -0600

    impl triangle

---
+++ haskell/triangle/Triangle.hs
@@ -0,0 +1,14 @@
+module Triangle (TriangleType(..), triangleType) where
+
+data TriangleType = Equilateral
+                    | Isosceles
+                    | Scalene
+                    | Illogical
+                    deriving (Show, Eq)
+
+triangleType :: Int -> Int -> Int ->  TriangleType
+triangleType a b c 
+  | a == b && b == c                 = Equilateral
+  | a+b <= c || b+c <= a || a+c <= b = Illogical
+  | a == b || b == c || a == c       = Isosceles
+  | otherwise                        = Scalene
