f500dda83d02a3809c44478d4a4c246c007a2e7d
Jon Schoning <jonschoning@gmail.com>
2014-02-22 17:34:21 -0600

    impl binary.hs

--- haskell/binary/Binary.hs
+++ haskell/binary/Binary.hs
@@ -1,4 +1,12 @@
 module Binary (toDecimal) where
 
+import Control.Applicative ((<$>),(<*>))
+import Data.Char (digitToInt)
+
 toDecimal :: String -> Integer
-toDecimal = undefined
+toDecimal s = if not (isValid s) then 0 else convert s
+  where 
+    isValid = all $ (||) <$> (=='1') <*> (=='0')
+    convert = toInteger . foldr (+) 0 . expandBases
+    expandBases = (zipWith (\b a -> digitToInt a * 2^b) [0..]) . reverse
+    
