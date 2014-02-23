0dead37d25d39ce29bcfcd1bac6f2b451c883f6e
Jon Schoning <jonschoning@gmail.com>
2014-02-23 00:07:21 -0600

    impl raindrops.hs

--- haskell/raindrops/Raindrops.hs
+++ haskell/raindrops/Raindrops.hs
@@ -2,21 +2,10 @@ module Raindrops (convert) where
 
 import Data.Monoid (mempty)
 
-rules :: [(Integer, String)]
 rules = [(3, "Pling"), (5, "Plang"), (7,"Plong")]
 
 convert :: Integer -> String
 convert n = if result == mempty then show n else result 
   where 
-    factors = primeFactors n
-    tryRule e s = if e `elem` factors then s else mempty
+    tryRule r s = if n `rem` r == 0 then s else mempty
     result = uncurry tryRule =<< rules
-
-primeFactors :: Integer -> [Integer]
-primeFactors = reverse . loop 2 []
-  where loop cur acc n 
-          | n == 1 = acc
-          | n `mod` cur == 0 = loop 2 (cur:acc) (quot n cur) 
-          | otherwise = loop (cur+1) acc n
-
-
