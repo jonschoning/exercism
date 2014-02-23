18eaae7e1da1c7df0446f7a5f9ceed16ac8f7fb9
Jon Schoning <jonschoning@gmail.com>
2014-02-23 15:46:40 -0600

    update Atbash.hs

--- haskell/atbash-cipher/Atbash.hs
+++ haskell/atbash-cipher/Atbash.hs
@@ -2,17 +2,12 @@
 
 module Atbash (encode) where
 
-import Data.List (intersperse)
+import Data.List.Split (chunksOf)
 import Data.Char (ord,chr,toLower,isAlphaNum,isDigit)
-import Data.Text (unpack,pack,chunksOf)
-import qualified Data.Text as T (concat)
 
 encode :: String -> String
-encode = unpack 
-         . T.concat
-         . intersperse " "
+encode = unwords
          . chunksOf 5
-         . pack
          . map update
          . filter isAlphaNum
   where update c 
