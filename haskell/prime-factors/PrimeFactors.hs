2f83ad2decd91df86f19cd4f86f016da1de1c5de
Jon Schoning <jonschoning@gmail.com>
2014-02-23 00:25:27 -0600

    upd primefactors

--- haskell/prime-factors/PrimeFactors.hs
+++ haskell/prime-factors/PrimeFactors.hs
@@ -4,6 +4,7 @@ primeFactors :: Integer -> [Integer]
 primeFactors = reverse . loop 2 []
   where loop cur acc n 
           | n == 1 = acc
+          | cur*cur > n = if n > 1 then n:acc else acc
           | n `mod` cur == 0 = loop 2 (cur:acc) (quot n cur) 
           | otherwise = loop (cur+1) acc n
 
