e86b797d527d520e0b3f567c6d1ea6efb6f2b520
Jon Schoning <jonschoning@gmail.com>
2014-02-23 04:05:37 -0600

    impl atbash.hs

--- haskell/atbash-cipher/Atbash.hs
+++ haskell/atbash-cipher/Atbash.hs
@@ -1,5 +1,21 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Atbash (encode) where
 
+import Data.List (intersperse)
+import Data.Char (ord,chr,toLower,isAlphaNum,isDigit)
+import Data.Text (unpack,pack,chunksOf)
+import qualified Data.Text as T (concat)
+
 encode :: String -> String
-encode = undefined
+encode = unpack 
+         . T.concat
+         . intersperse " "
+         . chunksOf 5
+         . pack
+         . map update
+         . filter isAlphaNum
+  where update c 
+          | isDigit c = c 
+          | otherwise = chr $ 219 - ord(toLower c)
 
