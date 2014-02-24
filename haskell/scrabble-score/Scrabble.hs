5b94bbf7855b19e780c49eb0df4d410649f2d4ed
Jon Schoning <jonschoning@gmail.com>
2014-02-23 19:42:37 -0600

    update scrabble-score.hs

--- haskell/scrabble-score/Scrabble.hs
+++ haskell/scrabble-score/Scrabble.hs
@@ -5,13 +5,9 @@ import Data.Char (toUpper)
 import qualified Data.Map.Strict as M
 
 letterValues :: M.Map Char Integer
-letterValues = M.fromList [  ('A',1), ('E',1), ('I',1), ('O',1), ('U',1), ('L',1), ('N',1), ('R',1), ('S',1), ('T',1)
-                           , ('D',2), ('G',2)
-                           , ('B',3), ('C',3), ('M',3), ('P',3)
-                           , ('F',4), ('H',4), ('V',4), ('W',4), ('Y',4)
-                           , ('K',5)
-                           , ('J',8), ('X',8)
-                           , ('Q',10), ('Z',10)]
+letterValues = M.fromList $ expandPairs [(1, "AEIOULNRST"), (2, "DG"), (3, "BCMP"), (4, "FHVWY"), (5, "K"), (8, "JX"), (10, "QZ")]
+  where expandPairs pair = pair >>= expandPair
+          where expandPair (i, s) = fmap (\c-> (c, i)) s
 
 scoreLetter :: Char -> Integer
 scoreLetter = fromJust . flip M.lookup letterValues . toUpper
