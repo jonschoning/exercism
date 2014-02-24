93ebca0dab2c4bc03d35fee2caf9fd1158f2e4b9
Jon Schoning <jonschoning@gmail.com>
2014-02-23 22:27:31 -0600

    update scrabble-score.hs

--- haskell/scrabble-score/Scrabble.hs
+++ haskell/scrabble-score/Scrabble.hs
@@ -2,12 +2,12 @@ module Scrabble (scoreLetter, scoreWord) where
 
 import Data.Maybe (fromJust)
 import Data.Char (toUpper)
-import qualified Data.Map.Strict as M
+import qualified Data.HashMap.Strict as M
 
-letterValues :: M.Map Char Integer
+letterValues :: M.HashMap Char Integer
 letterValues = M.fromList $ expandPairs [(1, "AEIOULNRST"), (2, "DG"), (3, "BCMP"), (4, "FHVWY"), (5, "K"), (8, "JX"), (10, "QZ")]
-  where expandPairs pair = pair >>= expandPair
-          where expandPair (i, s) = fmap (\c-> (c, i)) s
+  where expandPairs = (expandPair =<<)
+        expandPair (i, s) = fmap (\c-> (c, i)) s
 
 scoreLetter :: Char -> Integer
 scoreLetter = fromJust . flip M.lookup letterValues . toUpper
