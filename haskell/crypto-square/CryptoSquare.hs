f21a7d66cc704e5ba994ca81b0dd9a2d595a2a67
Jon Schoning <jonschoning@gmail.com>
2014-03-01 16:11:02 -0600

    use ceiling instead of find

--- haskell/crypto-square/CryptoSquare.hs
+++ haskell/crypto-square/CryptoSquare.hs
@@ -7,17 +7,16 @@ module CryptoSquare (normalizePlaintext,
 import Data.Char (toLower, isAlphaNum)
 import Data.List (find, transpose)
 import Data.List.Split (chunksOf)
-import Data.Maybe (fromMaybe)
 import Control.Applicative ((<$>),(<*>))
 
 normalizePlaintext :: String -> String
 normalizePlaintext = map toLower.filter isAlphaNum
 
 squareSize :: String -> Int
-squareSize s = fromMaybe 0 $ find (\i -> fromIntegral i >= sqrt (fromIntegral (length s))) [1..] 
+squareSize = ceiling . sqrt . fromIntegral . length
 
 plaintextSegments :: String -> [String]
-plaintextSegments = (chunksOf <$> squareSize <*> id) . normalizePlaintext 
+plaintextSegments = (chunksOf <$> squareSize <*> id) . normalizePlaintext
 
 ciphertext :: String -> String
 ciphertext = concat . transpose . plaintextSegments
