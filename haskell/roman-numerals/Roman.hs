f136107e1202ccb6ce6480a81fc11d76217c9195
Jon Schoning <jonschoning@gmail.com>
2014-02-28 23:31:22 -0600

    improve Romah.hs

--- haskell/roman-numerals/Roman.hs
+++ haskell/roman-numerals/Roman.hs
@@ -1,19 +1,13 @@
 module Roman (numerals) where
 
+import Data.Map.Strict as M
+import Data.List (unfoldr)
+import Control.Applicative ((<$>))
+
+conversions :: M.Map Int String
+conversions = M.fromList [(1000,"M"), (900,"CM"), (500,"D"), (400,"CD"), (100,"C"), (90,"XC"), (50,"L"), 
+                          (40,"XL"), (10,"X"), (9,"IX"), (5,"V"), (4,"IV"), (1,"I")]
+
 numerals :: Int -> String
-numerals i = numl i ""
-  where numl n s
-          | n == 0 = s
-          | n >= 1000 = numl (n-1000) (s++"M")
-          | n >= 900  = numl  (n-900) (s++"CM")
-          | n >= 500  = numl  (n-500) (s++"D")
-          | n >= 400  = numl  (n-400) (s++"CD")
-          | n >= 100  = numl  (n-100) (s++"C")
-          | n >= 90   = numl   (n-90) (s++"XC")
-          | n >= 50   = numl   (n-50) (s++"L")
-          | n >= 40   = numl   (n-40) (s++"XL")
-          | n >= 10   = numl   (n-10) (s++"X")
-          | n >= 9    = numl    (n-9) (s++"IX")
-          | n >= 5    = numl    (n-5) (s++"V")
-          | n >= 4    = numl    (n-4) (s++"IV")
-          | n <= 3    = numl    (n-1) (s++"I")
+numerals = concat . unfoldr convert
+  where convert n = (\(val, str) -> (str, n-val)) <$> M.lookupLE n conversions
