3be6455bf7fa58e56ba05f511405b07c214f2957
Jon Schoning <jonschoning@gmail.com>
2014-03-03 13:30:04 -0600

    upd garden.hs

--- haskell/kindergarten-garden/Garden.hs
+++ haskell/kindergarten-garden/Garden.hs
@@ -1,8 +1,8 @@
 module Garden (garden, defaultGarden, lookupPlants, Plant(..)) where
 
 import Data.Maybe (fromJust)
-import Data.List.Split
-import Data.List (sort)
+import Data.List.Split (chunksOf)
+import Data.List (sort, elemIndex)
 import Data.Char (ord)
 import Data.Tuple (swap)
 import Control.Arrow (second)
@@ -10,32 +10,29 @@ import Control.Arrow (second)
 data Plant = Radishes | Clover | Grass | Violets | None
              deriving (Show, Eq, Ord)
 
+data Garden = DefaultGarden [Plant] | NamedGarden [Plant] [String] 
+
 instance Enum Plant where
   fromEnum = fromJust . flip lookup plantEnum 
   toEnum = fromJust . flip lookup (map swap plantEnum)
+
 plantEnum = map (second ord) [(Radishes, 'R'), (Clover, 'C'), (Grass, 'G'), (Violets, 'V'), (None, '.')]
 
-garden :: [String] -> String -> [Plant]
-garden children g = concat $ foldl filler [[]] pairWithStartIndexes
-  where childStartIndexes = map childIndex (sort children)
-        gs = defaultGardens g
-        pairWithStartIndexes = zip childStartIndexes (chunksOf 2 (zip (head gs) (last gs)))
-        filler acc next 
-          | acc == [[]] = map padNone [fst gSquare, snd gSquare]
-          | otherwise = [head acc ++ padNone (fst gSquare), last acc ++ padNone (snd gSquare)]
-          where gSquare = unzip $ snd next
-                padNoneLength = (fst next) - length (head acc)
-                padNone = ((replicate padNoneLength None) ++)
+garden :: [String] -> String -> Garden
+garden = flip (NamedGarden . concat . defaultPlants)
 
-defaultGarden :: String -> [Plant]
-defaultGarden = concat . defaultGardens
+defaultGarden :: String -> Garden
+defaultGarden = DefaultGarden . concat . defaultPlants
 
-defaultGardens :: String -> [[Plant]]
-defaultGardens = map (map (toEnum . ord)) . lines
+defaultPlants :: String -> [[Plant]]
+defaultPlants = map (map (toEnum . ord)) . lines
 
-lookupPlants :: String -> [Plant] -> [Plant]
-lookupPlants child plants = take 2 . drop (childIndex child) =<< makeLists 
-  where makeLists = chunksOf =<< flip quot 2 . length $ plants
+lookupPlants :: String -> Garden -> [Plant]
+lookupPlants child (DefaultGarden plants)  = lookupPlants' (defaultChildIndex child) child plants
+  where defaultChildIndex = (2 *) . subtract (ord 'A') . ord . head
+lookupPlants child (NamedGarden plants names)  = lookupPlants' (namedChildIndex child (sort names)) child plants
+  where namedChildIndex n names = (2 *) $ fromJust $ elemIndex n names
 
-childIndex :: String -> Int
-childIndex = (2 *) . subtract (ord 'A') . ord . head
+lookupPlants' :: Int -> String -> [Plant] -> [Plant]
+lookupPlants' startIndex child plants = take 2 . drop startIndex  =<< makeLists plants
+  where makeLists = chunksOf =<< flip quot 2 . length
