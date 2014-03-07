da9a8ba52e895cbad35737029c92cb33d724fef1
Jon Schoning <jonschoning@gmail.com>
2014-03-07 16:43:09 -0600

    add base queens.hs

---
+++ haskell/queen-attack/Queens.hs
@@ -0,0 +1,11 @@
+module Queens (boardString, canAttack) where
+
+type Coord = (Integer, Integer)
+
+boardString :: Maybe Coord -> Maybe Coord -> String
+boardString = undefined
+
+canAttack :: Coord-> Coord -> Bool
+canAttack = undefined
+
+
