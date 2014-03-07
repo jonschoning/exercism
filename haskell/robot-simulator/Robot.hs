06f336d83b9c202de558cb2e6d3af540da210239
Jon Schoning <jonschoning@gmail.com>
2014-03-07 16:21:26 -0600

    impl robot.hs

--- haskell/robot-simulator/Robot.hs
+++ haskell/robot-simulator/Robot.hs
@@ -2,33 +2,49 @@ module Robot (Bearing(..), Robot, mkRobot,
               coordinates, simulate,
               bearing, turnRight, turnLeft) where
 
-data Bearing = North | South | East | West
-               deriving (Show, Eq, Enum)
+import Control.Monad (liftM2)
+import Data.List (foldl')
+
+data Bearing = North | East | South | West
+               deriving (Show, Eq, Enum, Ord, Bounded)
 
 type Coordinate = (Int, Int)
 
-data Robot = Robot
+data Robot = MkRobot { 
+                bearing :: Bearing,
+                coordinates :: Coordinate }
 
 instance Show Robot where
-  show r = undefined
+  show r = (show.bearing $ r) 
+           ++ " " ++ 
+           (show.coordinates $ r)
 
 instance Eq Robot where
-  (==) r1 r2 = undefined
+  (==) r1 r2 = (show r1) == (show r2)
 
 mkRobot :: Bearing -> Coordinate -> Robot 
-mkRobot = undefined
-
-coordinates :: Robot -> Coordinate
-coordinates = undefined
+mkRobot = MkRobot
 
 simulate :: Robot -> String -> Robot
-simulate = undefined
+simulate = foldl' step
+  where 
+    step r 'R' = r { bearing = turnRight.bearing $ r }
+    step r 'L' = r { bearing = turnLeft.bearing $ r }
+    step r 'A' = r { coordinates = liftM2 nextCoord bearing coordinates r }
+    step r _   = error "unknown command"
 
-bearing :: Robot -> Bearing
-bearing = undefined
+nextCoord :: Bearing -> Coordinate -> Coordinate
+nextCoord North (x, y) = (x, succ y)
+nextCoord South (x, y) = (x, pred y)
+nextCoord East (x, y) = (succ x, y)
+nextCoord West (x, y) = (pred x, y)
 
 turnRight :: Bearing -> Bearing
-turnRight = undefined
-
+turnRight b 
+  | b == maxBound = minBound
+  | otherwise = succ b
+            
 turnLeft :: Bearing -> Bearing
-turnLeft = undefined
+turnLeft b
+  | b == minBound = maxBound
+  | otherwise = pred b
