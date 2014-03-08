7c52f6f1be52238651b179c5c2760d398a3425fc
Jon Schoning <jonschoning@gmail.com>
2014-03-07 19:29:18 -0600

    add base BST

---
+++ haskell/binary-search-tree/BST.hs
@@ -0,0 +1,24 @@
+module BST (bstLeft, bstRight, bstValue, singleton, insert, fromList, toList) where
+
+data BST
+
+bstLeft :: BST -> Maybe BST
+bstLeft = undefined
+
+bstRight :: BST -> Maybe BST
+bstRight = undefined
+
+bstValue :: BST -> Int
+bstValue = undefined
+
+singleton :: Int -> BST
+singleton = undefined
+
+insert :: Int -> BST -> BST
+insert = undefined
+
+fromList :: [Int] -> BST
+fromList = undefined
+
+toList :: BST -> [Int] 
+toList = undefined
