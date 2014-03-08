43824c5bc3f80002237099509d69d07584bb2524
Jon Schoning <jonschoning@gmail.com>
2014-03-08 01:04:31 -0600

    add base largest-series-product

---
+++ haskell/largest-series-product/Series.hs
@@ -0,0 +1,10 @@
+module Series (digits, slices, largestProduct) where
+
+digits :: String -> [Int]
+digits = undefined
+
+slices :: Int -> String -> [[Int]]
+slices = undefined
+
+largestProduct :: Int -> String -> Int
+largestProduct = undefined
