8f1dd7475433a52e9232a771c4f6e4f61ac7bc13
Jon Schoning <jonschoning@gmail.com>
2014-03-08 14:54:47 -0600

    impl largest-series-product.hs

--- haskell/largest-series-product/Series.hs
+++ haskell/largest-series-product/Series.hs
@@ -1,10 +1,13 @@
 module Series (digits, slices, largestProduct) where
 
+import Data.Char (digitToInt)
+import Data.List (tails, product, maximum)
+
 digits :: String -> [Int]
-digits = undefined
+digits = map digitToInt
 
 slices :: Int -> String -> [[Int]]
-slices = undefined
+slices n = filter ((n==).length) . map (take n) . tails . digits
 
 largestProduct :: Int -> String -> Int
-largestProduct = undefined
+largestProduct i s = maximum $ map product $ slices i s
