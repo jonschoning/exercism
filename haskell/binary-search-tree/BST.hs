22fae60a1e245c91a30f20ffdcdefb962de4db22
Jon Schoning <jonschoning@gmail.com>
2014-03-07 23:34:39 -0600

    impl bst.hs

--- haskell/binary-search-tree/BST.hs
+++ haskell/binary-search-tree/BST.hs
@@ -1,24 +1,40 @@
 module BST (bstLeft, bstRight, bstValue, singleton, insert, fromList, toList) where
 
-data BST
+import Data.List (foldl')
+
+data Tree a = Tip | Node (Tree  a) a (Tree a)
+
+type BST = Tree Int
 
 bstLeft :: BST -> Maybe BST
-bstLeft = undefined
+bstLeft (Node a _ _) = Just a
+bstLeft _ = Nothing
 
 bstRight :: BST -> Maybe BST
-bstRight = undefined
+bstRight (Node _ _ a) = Just a
+bstRight _ = Nothing
 
 bstValue :: BST -> Int
-bstValue = undefined
+bstValue (Node _ a _) = a
+bstValue Tip  = error "no tip value"
 
 singleton :: Int -> BST
-singleton = undefined
+singleton a = Node Tip a Tip
 
 insert :: Int -> BST -> BST
-insert = undefined
+insert x (Tip) = error "can't insert tip"
+insert x (Node Tip a r) | x <= a = Node (singleton x) a r
+insert x (Node l a Tip) | x > a  = Node l a (singleton x)
+insert x (Node l a r)   | x <= a = Node (insert x l) a r
+insert x (Node l a r)   | x > a  = Node l a (insert x r)
 
 fromList :: [Int] -> BST
-fromList = undefined
+fromList [] = Tip
+fromList (x:xs) = foldl' (flip insert) (singleton x) xs
 
 toList :: BST -> [Int] 
-toList = undefined
+toList Tip = []
+toList (Node Tip a r) = a : toList r
+toList (Node l a Tip) = toList l ++ [a]
+toList (Node l a r)   = toList l ++ [a] ++ toList r
+
