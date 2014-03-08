63f3589d94faa14a3c8ae2f05d8a13ef093c5aee
Jon Schoning <jonschoning@gmail.com>
2014-03-08 14:59:50 -0600

    add base luhn.hs

---
+++ haskell/luhn/Luhn.hs
@@ -0,0 +1,17 @@
+module Luhn (checkDigit, addends, checksum, isValid, create) where
+
+checkDigit :: Integer -> Integer
+checkDigit = undefined
+
+addends :: Integer -> [Integer]
+addends = undefined
+
+checksum :: Integer -> Integer
+checksum = undefined
+
+isValid :: Integer -> Bool
+isValid = undefined
+
+create :: Integer -> Integer
+create = undefined
+
