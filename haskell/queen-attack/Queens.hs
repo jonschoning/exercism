0b3b2eb952d0872e9930baa136f43cceaf28c4a7
Jon Schoning <jonschoning@gmail.com>
2014-03-07 18:26:53 -0600

    impl queens.hs

--- haskell/queen-attack/Queens.hs
+++ haskell/queen-attack/Queens.hs
@@ -1,11 +1,24 @@
 module Queens (boardString, canAttack) where
 
+import Data.List (intersperse)
+import Data.List.Split (chunksOf)
+import qualified Data.Map.Strict as M
+
 type Coord = (Integer, Integer)
 
 boardString :: Maybe Coord -> Maybe Coord -> String
-boardString = undefined
+boardString w b = pretty . fmap getBoardChar $ allCoords
+  where 
+    allCoords =  [(x, y) | x <- [0..7], y <- [0..7]]
+    getBoardChar coord = M.findWithDefault 'O' coord board
+    pretty = unlines . map (intersperse ' ') . chunksOf 8
+    board = let b0 = M.empty
+                b1 = maybe b0 (\p -> M.insert p 'W' b0) w
+                b2 = maybe b1 (\p -> M.insert p 'B' b1) b
+             in b2
 
-canAttack :: Coord-> Coord -> Bool
-canAttack = undefined
-
-
+canAttack :: Coord -> Coord -> Bool
+canAttack (x1, y1) (x2, y2) = isRowCol || isDiag
+  where 
+    isRowCol = x1 == x2 || y1 == y2
+    isDiag = x1 - x2 == y1 - y2
