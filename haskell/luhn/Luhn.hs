7f4e9f89f16adb1a937ad1d26025358a7c93760f
Jon Schoning <jon.schoning@polarissolutions.com>
2014-03-10 15:58:12 -0500

    impl luhn.hs

--- haskell/luhn/Luhn.hs
+++ haskell/luhn/Luhn.hs
@@ -7,19 +7,25 @@ checkDigit :: Integer -> Integer
 checkDigit = (`mod` 10)
 
 addends :: Integer -> [Integer]
-addends = digits
+addends xs = zipWith go genCycle xstr
   where 
-    digits = foldr f (fromIntegral.digitToInt) . show
-    f i x = if even i then p (x*2) else x
-    p a = if a >= 10 then a - 9 else a
+    xstr = show xs
+    genCycle = drop (length xstr `mod` 2) (cycle [0, 1])
+    go isDouble xs = let x = (toInteger.digitToInt) xs 
+                         fix a = if a >= 10 then a - 9 else a
+                     in 
+                         if even isDouble then fix (x*2) else x
   
 
 checksum :: Integer -> Integer
-checksum = undefined
+checksum = checkDigit . sum . addends
 
 isValid :: Integer -> Bool
-isValid = undefined
+isValid = (0 ==) . checksum
 
 create :: Integer -> Integer
-create = undefined
+create n = (+ base) . (`mod` 10) . (10-) . checksum $ base
+  where 
+    base = n * 10
+
 
