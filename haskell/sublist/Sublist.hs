3cadde837772e5ce254d11ed86786ef7272c3fa9
Jon Schoning <jonschoning@gmail.com>
2014-07-16 01:09:54 -0500

    basic impl of sublist

--- haskell/sublist/Sublist.hs
+++ haskell/sublist/Sublist.hs
@@ -1,10 +1,17 @@
 module Sublist (Sublist(Equal, Sublist, Superlist, Unequal), sublist) where
 
+import Data.List (isInfixOf)
+
 data Sublist =  Equal 
               | Sublist 
               | Superlist 
               | Unequal deriving (Show, Eq)
 
 sublist :: (Eq a) => [a] -> [a] -> Sublist
-sublist = undefined
+sublist x y 
+  | (null x) && (null y) = Equal
+  | length x == length y && and (zipWith (==) x y) = Equal
+  | x `isInfixOf` y = Sublist
+  | y `isInfixOf` x = Superlist
+  | otherwise = Unequal
 
