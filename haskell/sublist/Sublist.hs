ffa877d828706778dec8d3fe512cc95ce1440d9d
Jon Schoning <jonschoning@gmail.com>
2014-07-16 01:31:22 -0500

    add sumofmult

--- haskell/sublist/Sublist.hs
+++ haskell/sublist/Sublist.hs
@@ -7,16 +7,16 @@ data Sublist =  Equal
               | Superlist 
               | Unequal deriving (Show, Eq)
 
-sublist :: (Eq a) => [a] -> [a] -> Sublist
+sublist :: Eq a => [a] -> [a] -> Sublist
 sublist xs ys 
   | isEqual True xs ys = Equal
-  | xs `isInfixOf` ys = Sublist
-  | ys `isInfixOf` xs = Superlist
-  | otherwise = Unequal
+  | xs `isInfixOf` ys  = Sublist
+  | ys `isInfixOf` xs  = Superlist
+  | otherwise          = Unequal
   where
-    isEqual False _      _      = False
-    isEqual True  []     []     = True
-    isEqual _     (a:as) []     = False
-    isEqual _     []     (b:bs) = False
-    isEqual _     (a:as) (b:bs) = isEqual (a == b) as bs
+    isEqual False _  _  = False
+    isEqual True  [] [] = True
+    isEqual _     _  [] = False
+    isEqual _     [] _  = False
+    isEqual _ (a:as) (b:bs) = isEqual (a == b) as bs
 
