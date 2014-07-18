5078c399ee248d6261fa321c8e769110c4a9220f
Jon Schoning <jonschoning@gmail.com>
2014-07-18 18:56:55 -0500

    add default sumofm

---
+++ haskell/sum-of-multiples/SumOfMultiples.hs
@@ -0,0 +1,7 @@
+module SumOfMultiples (sumOfMultiples, sumOfMultiplesDefault) where
+
+sumOfMultiplesDefault :: Integer -> Integer
+sumOfMultiplesDefault x = sumOfMultiples [3,5] x
+
+sumOfMultiples :: [Integer] -> Integer -> Integer
+sumOfMultiples ms x = undefined
