164083aada3ef593f2ecdfb027cef54790144540
Jon Schoning <jonschoning@gmail.com>
2014-07-18 19:37:31 -0500

    impl sum-of-mult

--- haskell/sum-of-multiples/SumOfMultiples.hs
+++ haskell/sum-of-multiples/SumOfMultiples.hs
@@ -1,7 +1,11 @@
 module SumOfMultiples (sumOfMultiples, sumOfMultiplesDefault) where
 
 sumOfMultiplesDefault :: Integer -> Integer
-sumOfMultiplesDefault x = sumOfMultiples [3,5] x
+sumOfMultiplesDefault = sumOfMultiples [3,5]
 
 sumOfMultiples :: [Integer] -> Integer -> Integer
-sumOfMultiples ms x = undefined
+sumOfMultiples ms x = sum $ takeWhile (< x) (multiples ms)
+
+multiples :: [Integer] -> [Integer]
+multiples ms = filter isValid [1..]
+  where isValid n = any (\a -> rem n a == 0) ms
